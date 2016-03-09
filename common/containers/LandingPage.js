d93a58fb96be27087f38fe73d2a69eae2df37cf5
Preethi <iam.preethi.k@gmail.com>
2016-03-08 23:28:24 -0800

    Add intro video on landing page

--- common/containers/LandingPage.js
+++ common/containers/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import RaisedButton from 'material-ui/lib/raised-button';
 import { connect } from 'react-redux';
+import YouTube from 'react-youtube';
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -20,10 +21,24 @@ export default class LandingPage extends Component {
         padding: 70,
         textAlign: 'center',
       },
+      playerStyle: {
+        position: 'relative',
+        textAlign: 'center',
+      },
     };
 
+    const opts = {
+      height: '400',
+      width: '600',
+      playerVars: {
+        autoplay: 0,
+      },
+    };
+
+    const id = '5BpDeQbZkOs';
+
     return (
-      <div>
+      <div className="container">
         <div style={styles.divStyle}>
           <h1>
             Know everything
@@ -37,6 +52,12 @@ export default class LandingPage extends Component {
               hoverColor="#FE4A49"
             />
           </ul>
+        </div>
+        <div style={styles.playerStyle}>
+          <YouTube
+            videoId={id}
+            opts={opts}
+          />
         </div>
       </div>
     );
